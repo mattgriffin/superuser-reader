a3047ef2c90cf1c4d13882012cae48b61f7ead03
Matt Griffin <me@mattgriffin.com>
2016-04-10 10:05:07 -0500

    Adds basic RSS feed
    Adds basic RSS feed from OpenStack Superuser.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,6 +41,21 @@ angular.module('starter.controllers', [])
   };
 })
 
+.controller("FeedController", function($http, $scope) {
+    $scope.init = function() {
+        $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://superuser.openstack.org/articles/feed/", "num": 100 } })
+            .success(function(data) {
+                $scope.rssTitle = data.responseData.feed.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;
+            })
+            .error(function(data) {
+                console.log("ERROR: " + data);
+            });
+    }
+})
+
 .controller('ArticlesCtrl', function($scope) {
   $scope.articles = [
     { title: 'Article 1', id: 1 },
